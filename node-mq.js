2e6ef0d2369485d537fdbe1b82c13b03388e0a66
Iain Emsley <iain_emsley@austgate.co.uk>
2013-03-07 22:08:09 +0000

    Notify function added

--- node-mq.js
+++ node-mq.js
@@ -1,31 +1,46 @@
-var http = require('http'),
-    util = require('util'),
-    url = require('url'), 
-    redis = require('redis'),
-    qs = require("querystring");
+var http = require('http'), 
+util = require('util'), 
+url = require('url'), 
+redis = require('redis'), 
+qs = require("querystring");
     
-
 try {
- redis.debug_mode = false;
- var redis_client = redis.createClient(); 
- var redis_produce = redis.createClient(); 
+	redis.debug_mode = false;
+    var redis_client = redis.createClient(); 
+    var redis_produce = redis.createClient(); 
 } catch (err) {
 	console.log('set up error: '+err);
 }
 
 var urlMap = {
-		'/consumer' : function (req, res) {
-	queue.consume(json, function (data) {
-	  res.simpleJSON(200, data);
-	});
+  '/consumer' : function (req, res) {
+	  var qtype = json.prefix.split("/");
+	  if (qtype[0] == "notify") {
+		  notify.consume(json, function (data) {
+			  res.simpleJSON(200, data);
+		  }); 
+	  } else {
+	      queue.consume(json, function (data) {
+		      res.simpleJSON(200, data);
+	      });
+	  }
 	},
-		'/producer' : function (req, res, json) {
-		queue.produce( json );
-		res.simpleJSON(200, {});
+	'/producer' : function (req, res, json) {
+	   //if transient - send to notify
+		var qtype = json.prefix.split("/");
+		if (qtype[0] == "notify") {
+			notify.produce(json);
+		} else {
+			queue.produce(json);
+		}
+	    res.simpleJSON(200, {});
 	}
 }
 
-var queue = new function () {
+/**
+ * Function to send a notification out via Pub/Sub. 
+ */
+var notify = new function () {
 	var callbacks=[];
 	this.consume = function (json, callback) {
        var b = [];
@@ -58,14 +73,35 @@ var queue = new function () {
 		  redis_produce.publish(json.queue, json.msg);
 		
 	} //end produce
+} //end notification
 
-
-} //end queue
+/**
+ * Function to send a message to a queue via a list
+ */
+var queue = new function () {
+	var callbacks=[];
+	this.consume = function (json, callback) {
+        var b = [];
+	    redis_client.on("error", function(err) {  console.log('Redis Error: '+ err); });
+	    
+	    if (b.length != 0) {
+			callback(b);
+		} else {
+			callbacks.push({ timestamp: new Date(), callback: callback });
+		};
+		console.log(callbacks);
+	    redis_client.blpop(json.queue);
+    }; //end consume
+	//function to push the message into the queue
+	this.produce = function(json) {
+		redis_produce.rpush(json.queue, json.msg);
+	}; //end produce
+}; //end queue
 
 
 http.createServer(function(req, res){
 	handler = urlMap[url.parse(req.url).pathname];
-	if (req.method=='POST') {
+	if (req.method=='POST') { ///POST should produce
 		req.body = '';
 
 		req.addListener('data', function (chunk) {
@@ -76,7 +112,7 @@ http.createServer(function(req, res){
 		  json = JSON.parse(req.body);
 		  handler(req, res, json);	
 		});
-	} else {
+	} else  { //change this so that a GET consumes only?
 		handler(req, res);
 		console.log("fell over");
 	}
@@ -86,8 +122,7 @@ http.createServer(function(req, res){
 		res.writeHead(code, {
 			"Content-Type": "text/json",
 			"Content-Length": body.length
-		}
-		);
+		});
 		res.end(body);
-		};
+	};
 }).listen(8003);
